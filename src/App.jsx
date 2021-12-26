8762897b74704f1dba05f95d4a9e0292a43cb97a
Carmen Liu <carmen.liujia@gmail.com>
2021-12-26 22:14:48 +1300

    Issue and airdrop ERC20-token

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 // import thirdweb
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
+
+const sdk = new ThirdwebSDK("rinkeby");
+// We can grab a reference to our ERC-1155 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x85FE621f95Ba6306eb84E2f9E5849158B236d728",
+);
+
+const tokenModule = sdk.getTokenModule(
+  "0xFd151ef8e9980d109C5E4D21158ed1d0554F9D85"
+);
 
 const App = () => {
   // Use the connectWallet hook thirdweb gives us.
   const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address)
-  const sdk = new ThirdwebSDK("rinkeby");
-  // We can grab a reference to our ERC-1155 contract.
-  const bundleDropModule = sdk.getBundleDropModule(
-    "0x85FE621f95Ba6306eb84E2f9E5849158B236d728",
-  );
 
-  const tokenModule = sdk.getTokenModule(
-    "0xFd151ef8e9980d109C5E4D21158ed1d0554F9D85"
-  );
 
   // The signer is required to sign transactions on the blockchain.
   // Without it we can only read data, not write.
@@ -27,12 +29,22 @@ const App = () => {
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
 
+  // Holds the amount of token each member has in state.
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  // The array holding all of our members addresses.
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  // A fancy function to shorten someones wallet address, no need to show the whole thing. 
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  };
+
   // Another useEffect!
   useEffect(() => {
     // We pass the signer to the sdk, which enables us to interact with
     // our deployed contract!
     sdk.setProviderOrSigner(signer);
-  }, [sdk, signer]);
+  }, [signer]);
 
   useEffect(() => {
     if (!address) {
@@ -55,7 +67,45 @@ const App = () => {
         setHasClaimedNFT(false);
         console.error("failed to nft balance", error);
       });
-  }, [address, bundleDropModule])
+  }, [address])
+
+
+  // This useEffect grabs all our the addresses of our members holding our NFT.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+    // with tokenId 0.
+    bundleDropModule
+      .getAllClaimerAddresses("0")
+      .then((addresess) => {
+        console.log("🚀 Members addresses", addresess)
+        setMemberAddresses(addresess);
+      })
+      .catch((err) => {
+        console.error("failed to get member list", err);
+      });
+  }, [hasClaimedNFT]);
+
+  // This useEffect grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Grab all the balances.
+    tokenModule
+      .getAllHolderBalances()
+      .then((amounts) => {
+        console.log("👜 Amounts", amounts)
+        setMemberTokenAmounts(amounts);
+      })
+      .catch((err) => {
+        console.error("failed to get token amounts", err);
+      });
+  }, [hasClaimedNFT]);
 
   const mintNft = () => {
     setIsClaiming(true);
@@ -78,6 +128,21 @@ const App = () => {
       });
   }
 
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          // If the address isn't in memberTokenAmounts, it means they don't
+          // hold any of our token.
+          memberTokenAmounts[address] || 0,
+          18,
+        ),
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
@@ -97,6 +162,29 @@ const App = () => {
       <div className="member-page">
         <h1>Coffee DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   };
